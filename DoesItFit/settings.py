d561d72e921478bd22bd78a23e52b56ee30dd60b
Nicolas Drufin <nicolas.drufin@ensc.fr>
2015-05-10 23:32:14 +0200

    change var

--- DoesItFit/settings.py
+++ DoesItFit/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'DoesItFit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'doesitfitdb',                      
+        'USER': 'root',                   
+        'PASSWORD': 'password',               
+        'HOST': 'localhost',                      
+        'PORT': '3306',                     
     }
 }
 
@@ -85,9 +89,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
