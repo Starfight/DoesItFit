50d139fc1206fc8817fa2944cc57146d2b4da605
Nicolas Drufin <nicolas.drufin@ensc.fr>
2015-05-14 18:07:18 +0200

    first version of site

--- DoesItFit/search/views.py
+++ DoesItFit/search/views.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class SearchView(TemplateView):
+
+    template_name="search_main.html"
