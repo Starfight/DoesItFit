2014f3a5fd9e212361f58305c3c9512d2549aa76
Nicolas Drufin <nicolas.drufin@ensc.fr>
2015-05-14 17:08:38 +0200

    add app search

--- DoesItFit/urls.py
+++ DoesItFit/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+admin.autodiscover()
+
+from DoesItFit.search.views import SearchView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', SearchView.as_view(), name='search'),
 ]
