26c2e4f8fa0ca2f2b1fffa1a3d56fd04844a9c96
Starfight <nicolas.drufin@ensc.fr>
2015-09-22 00:54:55 +0200

    Mise en place de la méthode post WIP

--- DoesItFit/search/views.py
+++ DoesItFit/search/views.py
@@ -23,6 +23,10 @@ class ResultsView(TemplateView):
     """
     template_name="search/results.html"
 
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return super(ResultsView, self).render_to_response(context)
+
     def get_context_data(self, **kwargs):
         """
         Envoi les infos contextuelles au gabarit
