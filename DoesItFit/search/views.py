e8cfff432240187759d52a7487a75cec19c5cfcb
Nicolas Drufin <nicolas.drufin@ensc.fr>
2016-10-24 00:47:02 +0200

    add get answer in view

--- DoesItFit/search/views.py
+++ DoesItFit/search/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from DoesItFit.search.forms import SearchForm
+from django.http import HttpResponseRedirect
 
 class SearchView(TemplateView):
     """
@@ -27,7 +28,32 @@ class ResultsView(TemplateView):
     def post(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
         return super(ResultsView, self).render_to_response(context)
-
+        
+    def get(self, request, *args, **kwargs):
+        """
+        GET methode
+        """
+        # vérifie qu'on est en GET
+        if request.method == 'GET':
+            # crée le formulaire à partir des données
+            form = SearchForm(request.GET)
+            # vérifie la validité
+            if form.is_valid():
+                # obtient les informations
+                lenght = form.cleaned_data['search_lenght']
+                width = form.cleaned_data['search_width']
+                height = form.cleaned_data['search_height']
+                subject = form.cleaned_data['search_subject']
+                
+                # requete les données
+                
+                context = self.get_context_data(**kwargs)
+                # return HttpResponse(form.cleaned_data['search_subject'])
+                return super(ResultsView, self).render_to_response(context)
+        
+        # erreur retour case départ
+        return HttpResponseRedirect("/")
+        
     def get_context_data(self, **kwargs):
         """
         Envoi les infos contextuelles au gabarit
